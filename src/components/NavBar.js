4d27bdf7b1c51f23e7cb87bffcc78ad1c2135077
Red <red.sarouf2014@gmail.com>
2023-07-07 00:48:28 -0400

    add react hooks class synthax lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Component } from "react/cjs/react.production.min";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-    </nav>
-  );
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
